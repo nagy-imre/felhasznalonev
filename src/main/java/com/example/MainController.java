1d71aea9eb598e99fe9c9ada8efb792d450c4f43
nagy-imre <nagyimre@ktch.hu>
2025-05-27 20:29:30 +0200

    MainController skeleton, mainScene kész

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,60 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MainController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button modifyButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private ListView<?> usernameListView;
+
+    @FXML
+    void onClickAboutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickAddButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickDeleteButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickModifyButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickedUsernameListView(MouseEvent event) {
+
+    }
+
+}
